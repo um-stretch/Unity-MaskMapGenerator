281f23abeb44056e873e05e15b459b8aab4e06eb
Stretch <90109041+um-stretch@users.noreply.github.com>
2026-06-06 06:52:36 -0400

    Improve error handling when making texture readable

--- Editor/Utilities.cs
+++ Editor/Utilities.cs
@@ -7,15 +7,24 @@ public static class Utilities
     public static Texture2D MakeReadable(Texture2D texture)
     {
         if (texture == null)
+        {
+            Debug.LogWarning("Input texture is null.");
             return null;
+        }
 
         string texPath = AssetDatabase.GetAssetPath(texture);
         if (string.IsNullOrEmpty(texPath))
+        {
+            Debug.LogWarning("Failed to find texture path.");          
             return null;
+        }
 
         var importer = AssetImporter.GetAtPath(texPath) as TextureImporter;
         if (importer == null)
+        {
+            Debug.LogWarning("Failed to get texture importer.");
             return null;
+        }
 
         if (!importer.isReadable)
         {
